b4f42c24a0e492305f35d320b2b865debeb419bd
Patrick Connolly <patrick@myplanetdigital.com>
2011-12-19 22:13:02 -0500

    Added small pennyworth_drupal recipe to add github fingerprint.

---
+++ cookbooks-sources/pennyworth_drupal/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Myplanet Digital, Inc."
+maintainer_email "devops@myplanetdigital.com"
+license          "Apache 2.0"
+description      "Installs/Configures pennyworth_drupal"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
